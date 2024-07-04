40fd26900799b0a351951914574e93e8d084eaa1
Sebastiaan <sdrinkenburg@rootsecurity.nl>
2024-07-04 17:31:57 +0200

    Update Storage

--- terraform/modules/StorageAccount/main.tf
+++ terraform/modules/StorageAccount/main.tf
@@ -6,6 +6,6 @@ resource "azurerm_storage_account" "STA" {
   account_replication_type = "LRS"
 
   tags = {
-    environment = "iac"
+    "mapping_tag" = "c600d5b1-a436-4f3d-87e9-9e2bb75f25a5"
   }
 }
