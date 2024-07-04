d2d3719148376eecc8cfd6a62f2f01550bb247a3
Sebastiaan <sdrinkenburg@rootsecurity.nl>
2024-07-04 19:16:14 +0200

    Container Create

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,23 +6,26 @@ terraform {
     key                  = "dev.terraform.tfstate"
   }
 }
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "ROOTSEC-RG" {
+  name     = "ROOTSEC-IAC-GITHUB"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestoraccount"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "ROOTSEC-STOR" {
+  name                     = "rootseciacstorage"
+  resource_group_name      = azurerm_resource_group.ROOTSEC-RG.name
+  location                 = azurerm_resource_group.ROOTSEC-RG.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+    tags = {
+    "mapping_tag" = "85f153b9-8a28-4592-98bc-ff839fd3ad4e"
+  }
 }
 
-resource "azurerm_storage_container" "example" {
-  name                  = "mycontainer"
-  storage_account_name  = azurerm_storage_account.example.name
-  container_access_type = "private"
+resource "azurerm_storage_container" "ROOTSEC-CON" {
+  name                  = "iacstorage"
+  storage_account_name  = azurerm_storage_account.ROOTSEC-STOR.name
+  container_access_type = "public"
 }
 
 
