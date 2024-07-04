1cbec16dc43645170190f038f5c356750317238f
Sebastiaan <sdrinkenburg@rootsecurity.nl>
2024-07-04 18:36:32 +0200

    fgdfsd

--- terraform/modules/Blob/main.tf
+++ terraform/modules/Blob/main.tf
@@ -1,5 +1,5 @@
-resource "azurerm_storage_container" "BLOB" {
-  name                     = "iacblob"
-  storage_account_name     = "rootseciacstorage"
-  container_access_type    = "private"
+resource azurerm_storage_container "myblobs" {
+  name                  = "myblobs"
+  storage_account_name  = "rootseciacstorage"
+  container_access_type = "private"
 }
